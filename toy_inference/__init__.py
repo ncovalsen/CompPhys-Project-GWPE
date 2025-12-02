7d59fb5631786ed5a99550ff5813121d7af0da40
Snehal Tibrewal <snehaltibrewal@Snehals-MacBook-Air.local>
2025-12-02 13:00:45 -0600

    fixed relative path in __init__.py

--- toy_inference/__init__.py
+++ toy_inference/__init__.py
@@ -4,8 +4,8 @@ This small package provides a worked example of Bayesian parameter
 estimation for a sine-Gaussian burst signal in Gaussian noise.
 """
 
-from .toy_inference.waveform import sine_gaussian
-from .toy_inference.data import simulate_sine_gaussian_data, SimulatedData, TrueParameters
-from .toy_inference.likelihood import log_likelihood, log_posterior, PriorBounds, DEFAULT_PRIOR
-from .toy_inference.mcmc import MCMCConfig, MCMCResult, run_mcmc
-from .toy_inference.config import ToyModelConfig
+from .waveform import sine_gaussian
+from .data import simulate_sine_gaussian_data, SimulatedData, TrueParameters
+from .likelihood import log_likelihood, log_posterior, PriorBounds, DEFAULT_PRIOR
+from .mcmc import MCMCConfig, MCMCResult, run_mcmc
+from .config import ToyModelConfig
